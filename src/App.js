e16ac995d003175d33d13fcf7d1217693efc29ae
Your Name <mohamedbalbola60@gmail.com>
2020-06-29 10:28:50 +0000

    dsadsa

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I was changed on the Feature branch and now we are trying to us volumes to show how that further goes
+          I was changed on the Feature branch
         </p>
       </div>
     );
